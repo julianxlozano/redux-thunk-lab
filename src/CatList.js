25dfe7bdb858aaf943d9febadffc11cda3307367
julianxlozano <julianxlozano@gmail.com>
2021-07-07 15:48:11 -0500

    commit tree 2c1acb801578098ccc3ab5bd65dca71a4875992c

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,14 @@
 // write your CatList component here
+import React from "react";
+
+
+ export const CatList = (props) =>  {
+    // debugger
+ 
+        return (
+            <div>
+                {props.catPics.map(catPic=><img src={catPic.url} alt='A Cat Pic'/>)}
+            </div>
+        );
+
+}
